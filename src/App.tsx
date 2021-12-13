3d1a6cee458d9754885cb90109e10fed92f0822f
John Setzer <john@mozartdata.com>
2021-12-13 14:06:01 -0500

    Added bug repro steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Reproduce Bug:</h1>
+      <ol>
+        <li>Hover over the div below.</li>
+        <li>Then check your browser console to see the warning.</li>
+      </ol>
+      <OverlayTrigger
+        placement="top"
+        delay={{ show: 250, hide: 250 }}
+        overlay={(injected) => (
+          <Tooltip id="tip" {...injected}>
+            <div style={{backgroundColor: 'yellow'}}>Check your browser console for `Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode.`</div>
+          </Tooltip>
+        )}
+      >
+        {({ ref, ...triggerHandler }) => (
+          <div ref={ref} {...triggerHandler}>Hover over me.</div>
+        )}
+      </OverlayTrigger>
     </div>
   );
 }
